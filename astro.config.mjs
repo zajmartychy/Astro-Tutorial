a56acd7d7feac3cb5bb48774bf592dfc790ca866
Marek Zajac <marek.zajac@adient.com>
2024-05-24 12:13:20 +0200

    complete!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({ site: "https://example.com" });
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [preact()]
+});
