8be605e541a7fa4f69de38bf727ebd774fa85d82
Marek Zajac <marek.zajac@adient.com>
2024-05-28 12:19:00 +0200

    SSR endpoints added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "hybrid",
+  adapter: netlify()
 });
